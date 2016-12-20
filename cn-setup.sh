4b55f9ad4857a7f16fec20631bb276d3f447d458
Thomas VARLET <thovarMS@users.noreply.github.com>
2016-12-20 11:49:59 +0100

    change the mount of /mnt/resource/scratch

--- cn-setup.sh
+++ cn-setup.sh
@@ -9,9 +9,9 @@ USER=`whoami`
 yum install -y -q nfs-utils
 mkdir -p /mnt/nfsshare
 # the NFS shared will be mounted on a persitant directory on the VM
-mkdir -p /mnt/scratch
+mkdir -p /mnt/resource/scratch
 chmod 777 /mnt/nfsshare
-chmod 777 /mnt/scratch
+chmod 777 /mnt/resource/scratch
 if [ "$LXDISTRO" == "CentOS-HPC" ] ; then
    if [ "$SKUNAME" == "6.5" ] ; then
    # For CentOS 6.5 (systemctl is supported for version > 7)
@@ -32,7 +32,7 @@ if [ "$LXDISTRO" == "CentOS-HPC" ] ; then
 fi   
 localip=`hostname -i | cut --delimiter='.' -f -3`
 echo "$IPPRE:/mnt/nfsshare    /mnt/nfsshare   nfs defaults 0 0" | tee -a /etc/fstab
-echo "$IPPRE:/mnt/resource/scratch    /mnt/scratch   nfs defaults 0 0" | tee -a /etc/fstab
+echo "$IPPRE:/mnt/resource/scratch    /mnt/resource/scratch   nfs defaults 0 0" | tee -a /etc/fstab
 showmount -e $IPPRE
 mount -a
 
@@ -42,7 +42,7 @@ if [ "$LXDISTRO" == "CentOS-HPC" ] ; then
 else
    rpm -v -i --nodeps /opt/intelMPI/intel_mpi_packages/*.rpm
 fi
-chown -R $USER:$USER /mnt/resource/
+chown -R $USER:$USER /mnt/resource/scratch
 
 # Don't require password for HPC user sudo
 echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
